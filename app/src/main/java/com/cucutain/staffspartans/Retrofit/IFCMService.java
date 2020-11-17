a7fee417239fcbea603e652ee506d0e7acdb412b
Jhon Arias <69868779+7honarias@users.noreply.github.com>
2020-11-16 19:46:15 -0500

    Update IFCMService.java
    delete key firebase

--- app/src/main/java/com/cucutain/staffspartans/Retrofit/IFCMService.java
+++ app/src/main/java/com/cucutain/staffspartans/Retrofit/IFCMService.java
@@ -8,10 +8,11 @@ import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
+//put firebase key
 public interface IFCMService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAA0W2hrPU:APA91bF-gVtaiQKbO992yxVoZwjYqVMLabGQZizy4-p54-21cGmFLIX0jlkKcYMmr9jHjNVywRJc7Z-n9dQzK9RctJaS-97f70u6HZ3f7A1oi0PcJwEWW4RrYUIMli_QGc0dxWr3ru7j"
+            "Authorization:key=keyFirebase"
 
     })
     @POST("fcm/send")
